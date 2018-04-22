bee150af05ba4a81a0ab2b043e4bb3e488501a74
Junaid Kokan <junaidkokan@flaregames.com>
2018-04-22 21:47:11 +0200

    change speed

--- js/app.js
+++ js/app.js
@@ -41,12 +41,6 @@ Enemy.prototype.render = function() {
 };
 
 
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -105,7 +99,7 @@ var enemy;
 allEnemies = [];
 for(var i = 0; i < enemyStarts.length; i = i+1)
 {
-  enemy = new Enemy(0, enemyStarts[i], 100 + Math.floor(Math.random() * 512));
+  enemy = new Enemy(0, enemyStarts[i], 100 + Math.floor(Math.random() * 500));
   allEnemies.push(enemy);
 }
 
